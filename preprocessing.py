c5141d67fffcb22276d0036bc3909301c1579159
coderpotter <anighojkar@usf.edu>
2021-01-02 18:44:12 -0500

    unzipping done

--- preprocessing.py
+++ preprocessing.py
@@ -3,36 +3,13 @@ from os import path, listdir, walk
 import sys
 
 
-def extract_files(filepath, extract_path, files_to_unzip, keep_zip):
-    '''
-    Unzips the files
-    '''
-    utils.unzip_files(filepath, extract_path, files_to_unzip, keep_zip)
-
-def list_files(directory, extension):
-    '''
-    input: directory path, file extension
-    ouput: list of files in the directory with the file extension
-    '''
-    for (dirpath, dirnames, filenames) in walk(directory):
-        return [f for f in filenames if f.endswith(extension)]
-
 if __name__ == "__main__":
-    '''
-    input: filepath, keep_zip, extract_all, files_to_unzip (if not extract_all), extract_here, extract_path (if not extract_here)
-    '''
+    """
+    input: filepath, extract_path (if not extract_here)
+    """
     filepath = sys.argv[1]
-    keep_zip = int(sys.argv[2])
-    x = 3
-    extract_all = int(sys.argv[3])
-    if not extract_all:
-        files_to_unzip = sys.argv[x+1].strip('[]').split(',')
-        x += 1
-    else:
-        files_to_unzip = list_files(filepath, '.zip')
-    extract_here = int(sys.argv[x+1])
-    extract_path = filepath
-    if not extract_here:
-        x += 1
-        extract_path = sys.argv[x+1]
-    extract_files(filepath, extract_path, files_to_unzip, keep_zip)
+    try:
+        extract_path = sys.argv[2]
+    except:
+        extract_path = None
+    utils.unzip_files(path=filepath, extract_to=extract_path, files_to_unzip=utils.list_files(directory=filepath, extension=".zip"))
--- utils.py
+++ utils.py
@@ -1,28 +1,49 @@
 import zipfile
-import os.path
+import os
 
 
-def unzip(filepath, directory_to_extract_to):
+def unzip(filepath, extract_to):
+    """
+    filepath: path to the file to unzip
+    extract_to: directory/path to unzip the files at
+    """
     with zipfile.ZipFile(filepath, 'r') as zip_ref:
-        zip_ref.extractall(directory_to_extract_to)
+        zip_ref.extractall(extract_to)
 
 
-def unzip_files(path, extract_to, files_to_unzip, keep_zip):
-    
-    
-        for filename in files_to_unzip:
-            try:
-                # .zip file exists and .csv doesn't
-                if not os.path.isfile(extract_to + os.path.splitext(filename)[0]) and os.path.isfile(path + filename):
-                    unzip(path + filename , extract_to)
-                    print(filename, "unzipped")
-                # .zip and .csv files don't file exist
-                elif not os.path.isfile(extract_to + os.path.splitext(filename)[0]) and not os.path.isfile(path + filename):
-                    print("{} Not Found".format(filename))
-                
-                if not keep_zip:
-                    os.remove(path + filename)
-            except FileNotFoundError:
-                print("Wrong file or file path")
-                continue
+def list_files(directory=".", extension=""):
+    """
+    input: directory path, file extension
+    output: list of files in the directory with the file extension
+    """
+    for (_, _, filenames) in os.walk(directory):
+        return [f for f in filenames if f.endswith(extension)]
+
+
+def unzip_files(path=".", extract_to=None, files_to_unzip=[], keep_zips=False):
+    """
+    path: path to zip files
+    extract_to: directory/path to unzip the files at (if None, extracts to `path`)
+    files_to_unzip: list of files to unzip
+    keep_zips: whether to keep the .zip files
+    """
+    path = os.path.abspath(path)
+    if not extract_to:
+        extract_to = path
+    else:
+        extract_to = os.path.abspath(extract_to)
+    for filename in files_to_unzip:
+        try:
+            if not os.path.isfile(os.path.join(extract_to, os.path.splitext(filename)[0])): # unzipped files do not exist
+                if os.path.isfile(os.path.join(path, filename)): # .zip files exist
+                    unzip(os.path.join(path, filename), extract_to)
+                else: # .zip files do not exist
+                    print("{} not found".format(filename))
+
+            if not keep_zips:
+                os.remove(os.path.join(path, filename))
+        
+        except FileNotFoundError:
+            print("Wrong file or file path: {}".format(filename))
+            continue
 
