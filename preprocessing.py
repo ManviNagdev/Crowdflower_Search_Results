870738325c97c8ce9d5c68ced20c29a7f349185a
ManviNagdev <manvinagdev@gmail.com>
2021-01-03 20:16:14 -0500

    convert html to text, remove stop words, lemmatize

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,8 @@
 import utils
 from os import path, listdir, walk
 import sys
-
+import pandas as pd
+from tqdm import tqdm
 
 if __name__ == "__main__":
     """
@@ -12,4 +13,35 @@ if __name__ == "__main__":
         extract_path = sys.argv[2]
     except:
         extract_path = None
-    utils.unzip_files(path=filepath, extract_to=extract_path, files_to_unzip=utils.list_files(directory=filepath, extension=".zip"))
+    utils.unzip_files(
+        path=filepath,
+        extract_to=extract_path,
+        files_to_unzip=utils.list_files(directory=filepath, extension=".zip"),
+    )
+
+    # load dataset
+    train = pd.read_csv("D:/Machine Learning/Crowdflower_Search_Results/data/train.csv")
+    test = pd.read_csv("D:/Machine Learning/Crowdflower_Search_Results/data/test.csv")
+
+    # drop NaN values
+    train.dropna(inplace=True)
+
+    # convert html text to normal text
+    train["product_description"] = train["product_description"].apply(utils.html2text)
+    print(train.shape, train.head(), sep="\n")
+    for i, row in tqdm(train.iterrows()):
+        train.loc[i, "product_description"] = utils.remove_stopwords_without_tokenize(
+            row["product_description"]
+        )
+    train.to_csv(
+        "D:/Machine Learning/Crowdflower_Search_Results/preprocessed_data/removed_stopwords.csv"
+    )
+
+    # lemmatize
+    for i, row in tqdm(train.iterrows()):
+        train.loc[i, "product_description"] = utils.lemmatize(
+            row["product_description"]
+        )
+    train.to_csv(
+        "D:/Machine Learning/Crowdflower_Search_Results/preprocessed_data/lemmatized.csv"
+    )
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import zipfile
 import os
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download("punkt")
+nltk.download("wordnet")
+nltk.download("stopwords")
+wordnet_lemmatizer = WordNetLemmatizer()
 
 
 def unzip(filepath, extract_to):
@@ -34,7 +44,9 @@ def unzip_files(path=".", extract_to=None, files_to_unzip=[], keep_zips=False):
         extract_to = os.path.abspath(extract_to)
     for filename in files_to_unzip:
         try:
-            if not os.path.isfile(os.path.join(extract_to, os.path.splitext(filename)[0])):  # unzipped files do not exist
+            if not os.path.isfile(
+                os.path.join(extract_to, os.path.splitext(filename)[0])
+            ):  # unzipped files do not exist
                 if os.path.isfile(os.path.join(path, filename)):  # .zip files exist
                     unzip(os.path.join(path, filename), extract_to)
                 else:  # .zip files do not exist
@@ -46,3 +58,46 @@ def unzip_files(path=".", extract_to=None, files_to_unzip=[], keep_zips=False):
         except FileNotFoundError:
             print("Wrong file or file path: {}".format(filename))
             continue
+
+
+def html2text(html_text):
+    """
+    html_text: text along with HTML formatting tags
+    """
+    soup = BeautifulSoup(html_text, "html.parser")
+    return soup.get_text()
+
+
+def remove_stopwords_without_tokenize(text):
+    """
+    text: string from which stopwords are to be removed
+    """
+    text_without_sw = [
+        word for word in text.split(" ") if not word in stopwords.words("english")
+    ]
+    return (" ").join(text_without_sw)
+
+
+def remove_stopwords_with_tokenize(text):
+    """
+    text: string from which stopwords are to be removed
+    """
+    text_tokens = word_tokenize(text)
+    text_without_sw = [
+        word for word in text_tokens if not word in stopwords.words("english")
+    ]
+    return (" ").join(text_without_sw)
+
+
+def lemmatize(text):
+    """
+    text: string to be lemmatized
+    """
+    punctuations = "?:!.,;"
+    text_tokens = word_tokenize(text)
+    lemmatized_text = [
+        wordnet_lemmatizer.lemmatize(word, pos="v")
+        for word in text_tokens
+        if not word in punctuations
+    ]
+    return (" ").join(lemmatized_text)
